ead69ba6dd61934f0f949fd184e76e2ce5572589
palani3 <palanik321@gmail.com>
2022-11-28 22:48:26 +0530

    Login and Register

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,10 +1,28 @@
+import axios from 'axios'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 // import logo from "../assets/logo.jpg";
 import logo from '../../assets/logo.jpg'
 import Navbar from '../Navbar'
 
+
 export default function Login() {
+ const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+    
+
+    const loginForm = (data)=>{
+        console.log(JSON.stringify(data));
+        
+
+    axios.post('http://localhost:5000/api/user/signin',data).then((res)=>{console.log("datas",res)})
+       
+    }
+
   return (
     <div>
         <section className="flex bg-gray-50 dark:bg-gray-900">
@@ -21,14 +39,16 @@ export default function Login() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={handleSubmit(loginForm)} className="space-y-4 md:space-y-6" >
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      <input {...register('email',{required:true})} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      {errors.email && <p>Email  is required.</p>}
                   </div>
                   <div>
                       <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <input {...register('password',{required:true})}  type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      {errors.password && <p>password  is required.</p>}
                   </div>
                   <div className="flex items-center justify-between">
                       <div className="flex items-start">
--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,9 +1,30 @@
+import axios from 'axios'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.jpg'
 import Navbar from '../Navbar'
 
+
+
 export default function Register() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+
+      const onSubmit =async (data) => {
+        console.log(data);
+        const response=await axios.post('http://localhost:5000/api/user/signup',data)
+        if(response.status==200)
+        {
+          
+          
+            console.log("response",response.data);
+        }
+       
+      };
   return (
     <div>
         <section className="flex bg-gray-50 dark:bg-gray-900">
@@ -14,33 +35,44 @@ export default function Register() {
           <img className="w-12 h-12 mr-2 rounded-full" src={logo} alt="logo" />
             
       </a>
-      <p className='font-semibold text-gray-900 dark:text-white mb-5 flex items-center text-center text-2xl'>Welcome to Movie Rules</p>
+      
+            <p className='font-semibold text-gray-900 dark:text-white mb-5 flex items-center text-center text-2xl'>Welcome to Movie Rules</p>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Create and account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={handleSubmit(onSubmit)}  className="space-y-4 md:space-y-6" >
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      <input   {...register('email',{ required: true })} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      {errors.email && <p className='text-red-600'>Email is required.</p>}
+                  </div>
+                  <div>
+                      <label htmlFor="Username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your UserName</label>
+                      <input   {...register('username',{ required: true })} type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="UserName" required=""/>
+                      {errors.username && <p className='text-red-600'>UserName is required.</p>}
                   </div>
                   <div>
                       <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <input  {...register('password', { required: true })} type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      {errors.password && <p>Password is required.</p>}
                   </div>
-                  <div>
+                  {/* <div>
                       <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                       <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div className="flex items-start">
+                    
+                  </div> */}
+
+                  {/* <div className="flex items-start">
                       <div className="flex items-center h-5">
                         <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                       </div>
                       <div className="ml-3 text-sm">
                         <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                       </div>
-                  </div>
+                  
+                  </div> */}
                   <button type="submit" className="w-full text-white bg-blue-600 hover:bg-Emerald-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Already have an account? <Link to="/Login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
@@ -48,6 +80,7 @@ export default function Register() {
               </form>
           </div>
       </div>
+
   </div>
 </section>
     </div>
