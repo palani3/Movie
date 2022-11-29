df16adca9721f044974c7490e700bc5bd658226e
palani3 <palanik321@gmail.com>
2022-11-29 21:41:41 +0530

    Private for Jwt Token

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import React from "react";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Home from "./Components/Home";
-import { Routes, Route } from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import Newmovies from "./pages/Newmovies";
+import PrivateRoute from "./utilis/PrivateRoute";
+
 
 export default function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Router>   */}
+            <Routes>
         <Route path="/" element={ <Navbar/> } />
         <Route path="/Login" element={ <Login/> } />
         <Route path="/Register" element={ <Register/> } />
-
+        <Route element={<PrivateRoute/>}>
         <Route path="about" element={ <About/> } />
-        <Route path="contact" element={ <Contact/> } />
         <Route path="Newmovies" element={ <Newmovies/> } />
+        </Route>
+
+        
+        <Route path="contact" element={ <Contact/> } />
+        
       </Routes>
-      {/* <Navbar/> */}
+      {/* </Router> */}
+
+            {/* <Navbar/> */}
       {/* <Login/>
       <Register/>
       <Home/>
--- src/Components/Route/privateroute.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function privateroute() {
-  return (
-    <div>privateroute</div>
-  )
-}
