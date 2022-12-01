52ff77a2c9ca3e16d5f4b35ef074289062c9f330
palani3 <palanik321@gmail.com>
2022-12-01 22:25:34 +0530

    Trending

--- src/pages/TrendingMovies.jsx
+++ src/pages/TrendingMovies.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
 
 export default function TrendingMovies() {
   return (
-    <div>TrendingMovies</div>
-  )
+    <div>
+      <section className="flex bg-gray-50 dark:bg-gray-900">
+        <Navbar />
+        <div className=" justify-cente pl-9">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm max-h-96">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                alt=""
+              />
+            </a>
+           
+          </div>
+
+        </div>
+      </section>
+    </div>
+  );
 }
