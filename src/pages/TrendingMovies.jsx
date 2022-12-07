dad8947a897ea408d64fbc881f70474446f68d0a
palani3 <palanik321@gmail.com>
2022-12-07 23:16:13 +0530

    Trending

--- src/pages/TrendingMovies.jsx
+++ src/pages/TrendingMovies.jsx
@@ -17,7 +17,7 @@ export default function TrendingMovies() {
             </a>
            
           </div>
-          <p>Im palani i will get 10LP more enjoy the ride maga you will achive</p>
+          <p>Im palani sad will get 10LP more enjoy the ride maga you will achive</p>
 
         </div>
       </section>
